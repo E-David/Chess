f94d68c406dc8123809618c2db62af0ee393e056
E-David <ericdavid365@gmail.com>
2016-03-19 17:35:38 -0500

    Commit before changing Chess Piece Format

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,19 +1,36 @@
 module Chess
 	class Pawn
-		attr_reader :color, :position, :alive, :possible_movement
-		def initialize(color, position, alive = 1)
+		attr_reader :color, :position, :valid_moves
+		def initialize(color, position)
 			@color = color
 			@position = position
-			@alive = alive
-			@possible_movement = [[1,-1],[1,0],[2,0],[1,1]]
+			@valid_moves
+		end
+
+		def base_movement
+			if color == "black"
+				@valid_moves << [1,0]
+			elsif color == "white"
+				@valid_moves << [-1,0]
+			end
+		end
+
+		def diagonals
+
 		end
 
 		#creates new array of each possible movement plus the current position
 		def available_moves
-			possible_movement.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
+			possible_moves = possible_movement
+			possible_moves.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
 		end
+
+		def validate_move
+			available_moves.select { |move| move.all? { |x| x >= 0 && x <= 7 } }
+		end
+
 	end
 end
 
-x = Chess::Pawn.new("black",[1,0])
-p x.available_movess
+x = Chess::Pawn.new("black",[1,1])
+p x.validate_move
