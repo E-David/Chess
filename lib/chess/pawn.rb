c4d3cd39fb330643797ca60942635cfded42c823
E-David <ericdavid365@gmail.com>
2016-03-19 14:55:53 -0500

    minor comment before renaming branch

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,18 +1,19 @@
 module Chess
 	class Pawn
-		attr_reader :color, :position, :alive, :possible_moves
+		attr_reader :color, :position, :alive, :possible_movement
 		def initialize(color, position, alive = 1)
 			@color = color
 			@position = position
 			@alive = alive
-			@possible_moves = [[1,-1],[1,0],[2,0],[1,1]]
+			@possible_movement = [[1,-1],[1,0],[2,0],[1,1]]
 		end
 
+		#creates new array of each possible movement plus the current position
 		def available_moves
-			possible_moves.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
+			possible_movement.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
 		end
 	end
 end
 
 x = Chess::Pawn.new("black",[1,0])
-p x.available_moves
+p x.available_movess
