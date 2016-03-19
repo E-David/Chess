9e98f6612a168f489bf5176e919749a8346b454a
E-David <ericdavid365@gmail.com>
2016-03-18 19:33:43 -0500

    add available moves method

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,10 +1,18 @@
 module Chess
 	class Pawn
-		attr_reader :color, :position, :alive
+		attr_reader :color, :position, :alive, :possible_moves
 		def initialize(color, position, alive = 1)
 			@color = color
 			@position = position
 			@alive = alive
+			@possible_moves = [[1,-1],[1,0],[2,0],[1,1]]
+		end
+
+		def available_moves
+			possible_moves.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
 		end
 	end
 end
+
+x = Chess::Pawn.new("black",[1,0])
+p x.available_moves
