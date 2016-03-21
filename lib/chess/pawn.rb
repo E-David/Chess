11081a16c96b96a18055fea1f71777e068729166
E-David <ericdavid365@gmail.com>
2016-03-20 19:26:47 -0500

    add minor chess piece class and subclasses format

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,32 +1,40 @@
 module Chess
-	class Pawn
-		attr_reader :color, :position, :valid_moves
-		def initialize(color, position)
+	class Piece
+		attr_reader :color, :position, :move_from, :move_to
+		def initialize(color, move_from, move_to)
 			@color = color
 			@position = position
-			@valid_moves
+			@move_from = move_from
+			@move_to = move_to
 		end
 
-		def base_movement
-			if color == "black"
-				@valid_moves << [1,0]
-			elsif color == "white"
-				@valid_moves << [-1,0]
+		class Pawn << Piece
+			attr_reader :valid_moves
+			def initialize
+				@valid_moves = valid_moves
 			end
-		end
 
-		def diagonals
+			def base_movement
+				if color == "black"
+					@valid_moves << [1,0]
+				elsif color == "white"
+					@valid_moves << [-1,0]
+				end
+			end
 
-		end
+			def diagonals
+				
+			end
 
-		#creates new array of each possible movement plus the current position
-		def available_moves
-			possible_moves = possible_movement
-			possible_moves.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
-		end
+			#creates new array of each possible movement plus the current position
+			def available_moves
+				possible_moves = possible_movement
+				possible_moves.map! { |move| [move,position].transpose.map { |t| t.reduce(:+) } }
+			end
 
-		def validate_move
-			available_moves.select { |move| move.all? { |x| x >= 0 && x <= 7 } }
+			def validate_move
+				available_moves.select { |move| move.all? { |x| x >= 0 && x <= 7 } }
+			end
 		end
 
 	end
